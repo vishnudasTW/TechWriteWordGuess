8b1fe319a7e9fe10011c823b50da36edb75e29f2
Vishnu Das <92569350+vishnudasTW@users.noreply.github.com>
2024-07-15 21:24:20 +0530

    Add files via upload

---
+++ word.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word: "Concise",
+        hint: "Primary goal of technical documentation - Clear & ____ documentation"
+    },
+    {
+        word: "Index",
+        hint: "Essential element for navigating a user guide?"
+    },
+    {
+        word: "Consistancy",
+        hint: "Benefit of using a single-source content management system"
+    },
+    {
+        word: "Present",
+        hint: "Technical writing should be written in______ tense"
+    }, 
+    {
+        word: "Development",
+        hint: "Document __________ Life Cycle"
+    },
+]
